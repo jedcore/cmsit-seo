19391172b86a61eef3320d14b1b41373c8a23337
jedcore <jedcore14@gmail.com>
2016-02-03 12:22:21 +0800

    Updated Footer BG

--- footer.php
+++ footer.php
@@ -61,7 +61,7 @@
     <div class="container">
       <div class="row">
         <div class="sec-title">
-          <h2>Our case studies</h2>
+          <h2>Our Case Studies</h2>
         </div><!--//sec-title-->
 
         <div class="case-item">
